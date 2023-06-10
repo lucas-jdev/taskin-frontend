21f4291ba9def4224fc791a30a189cfcfcaeb3de
Lucas Jdev <lucas.jdev1@gmail.com>
2023-06-10 11:09:13 -0300

    response without await

--- src/pages/task/form/index.tsx
+++ src/pages/task/form/index.tsx
@@ -49,7 +49,7 @@ export default function TaskForm() {
          
         await fetch(endpoint, options)
             .then(async(response) => {
-                const dataJSON = JSON.stringify(await response.json())
+                const dataJSON = JSON.stringify(response.json())
                 toast.info(JSON.parse(dataJSON))
                 if (response.status >= 400) {
                     const err = JSON.parse(dataJSON)

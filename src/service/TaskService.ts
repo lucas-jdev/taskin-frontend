f302f204f00c54cba6c2b65006622722c422ccf2
Lucas Jdev <lucas.jdev1@gmail.com>
2023-06-11 22:15:32 -0300

    all promises

--- src/service/TaskService.ts
+++ src/service/TaskService.ts
@@ -1,15 +1,13 @@
 import { Status } from "@/models/Task";
 
-export const deleteTask = (id: string) => {
-    fetch(`https://taskin-backend-production.up.railway.app/api/task/${id}`, {
+export const deleteTask = async (id: string) => {
+    await fetch(`https://taskin-backend-production.up.railway.app/api/task/${id}`, {
         method: 'DELETE'
-    }).catch((error) => {
-        console.error('Error:', error);
     });
 };
 
-export const archiveTask = (id: string) => {
-    fetch(`https://taskin-backend-production.up.railway.app/api/task/${id}`, {
+export const archiveTask = async (id: string) => {
+    await fetch(`https://taskin-backend-production.up.railway.app/api/task/${id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
@@ -17,7 +15,5 @@ export const archiveTask = (id: string) => {
         body: JSON.stringify({
             status: Status.ARCHIVED
         }),
-    }).catch((error) => {
-        console.error('Error:', error);
     });
 };
